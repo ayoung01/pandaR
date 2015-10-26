75fe573172cae04cf9fc8e52c3bfbe872b01ae78
jnpaulson <nosson@gmail.com>
2015-10-26 11:12:31 -0400

    adding unit test for panda algorithm

--- inst/unitTests/test_panda_r_vs_c.R
+++ inst/unitTests/test_panda_r_vs_c.R
@@ -1,13 +1,10 @@
-test_panda_r_vs_c <- function() {
+test_panda_r_vs_c <- function(){
   data("pandaToyData")
   panda.r <- with(pandaToyData, panda(motif, expression, ppi))
-  reg.r <- melt(panda.r@regNet)
+  reg.r <- reshape::melt.array(slot(panda.r,"regNet"))
   colnames(reg.r) <- c("TF", "Gene", "Z")
   reg.r <- reg.r[with(reg.r, order(TF, Gene)), ]
   
-  reg.c <- read.delim("data/toy_FinalNetwork.pairs")[, c(1,2,4)]
-  colnames(panda.c) <- c("TF", "Gene", "Z")
-  reg.c <- reg.c[with(reg.c, order(TF, Gene)), ]
-  
-  checkTrue(1 - cor(reg.c$Z, reg.r$Z, method="Spearman") < 1e-10)
+  data(pandaResultPairs)
+  checkTrue( (1 - cor(pandaResultPairs$Z, reg.r$Z, method="spearman")) < 1e-10)
 }
