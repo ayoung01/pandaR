839cb0575d8430ddc6c8c12157ea1edcaeeb9561
Albert Young <albert@alberts-mbp-3.dfci.harvard.edu>
2015-11-18 10:41:32 -0500

    fixed test_removemissing and changed data frames in pandaToyData from class factor to character

--- inst/unitTests/test_removemissing.R
+++ inst/unitTests/test_removemissing.R
@@ -2,21 +2,15 @@ test_removemissing <- function(){
   data(pandaToyData)
   motif <- pandaToyData$motif
   ppi <- pandaToyData$ppi
-  # annoyingly, columns of pandaToyData are of class factor
-  motif$V1 <- as.character(motif$V1)
-  motif$V2 <- as.character(motif$V2)
-  motif$V3 <- as.numeric(motif$V3)
-  
-  ppi$V1 <- as.character(ppi$V1)
-  ppi$V2 <- as.character(ppi$V2)
-  ppi$V3 <- as.numeric(ppi$V3)
   
   # add motif edges that target genes missing in the expression data
   motif <- rbind(motif, c("AHR", "MISSING_GENE", 1))
+  motif[,3] <- as.numeric(motif[,3])
   # add PPI edges between TFs not present in the motif
   ppi <- rbind(ppi, c("AHR", "MISSING_TF1", 1))
   ppi <- rbind(ppi, c("MISSING_TF2", "AHR", 1))
   ppi <- rbind(ppi, c("MISSING_TF1", "MISSING_TF3", 1))
+  ppi[,3] <- as.numeric(ppi[,3])
   panda.r <- panda(motif, pandaToyData$expression, ppi,
                    edgelist=TRUE,
                    remove.missing.ppi=TRUE,
@@ -26,5 +20,5 @@ test_removemissing <- function(){
   checkTrue(class(reg.r)=="data.frame")
   checkTrue(all(reg.r$TF%in%pandaToyData$motif[,1]))
   checkTrue(all(!reg.r$TF%in%c("MISSING_TF1","MISSING_TF2","MISSING_TF3")))
-  checkTrue(all(!reg.r$Gene%in%pandaToyData$motif[,2]))
+  checkTrue(all(reg.r$Gene%in%pandaToyData$motif[,2]))
 }
